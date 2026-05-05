540876a30268820340643176ddb465e6eab1ab11
dannydecoy <gyebidaniel428@gmail.com>
2026-05-05 13:47:58 +0000

    added expense tracker API skeleton

--- expense-tracker/app.py
+++ expense-tracker/app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+# Temporary storage (we'll upgrade this later)
+expenses = []
+
+
 @app.route('/')
 def home():
-    return "Expense Tracker is running!"
+    return jsonify({"message": "Expense Tracker is running!"})
+
+@app.route('/expenses', methods=['GET'])
+def get_expenses():
+     return jsonify({"expenses": expenses})
+
+@app.route('/expense', methods=['POST'])
+def add_expense():
+    data = request.get_json()
+    expense = {
+        "id": len(expenses) + 1,
+        "name": data["name"],
+        "amount": data["amount"]
+    }
+    expenses.append(expense)
+    return jsonify({"message": "Expense added!", "expense": expense})
+
+
+@app.route('/expense/<int:id>', methods=['DELETE'])
+def delete_expense(id):
+    global expenses
+    expenses = [e for e in expenses if e["id"] != id]
+    return jsonify({"message": f"Expense {id} deleted!"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
